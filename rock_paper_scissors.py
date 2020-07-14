d7a31853e15226824a6ef405e8e6b69caa8f739d
Daryl Galvez <daryl.kevin.24@gmail.com>
2020-07-14 20:39:54 +0800

    Initial Commit: Layout basics for RPS game

---
+++ rock_paper_scissors.py
@@ -0,0 +1,10 @@
+"""This program plays a game of Rock, Paper, Scissors between two Players,
+and reports both Player's scores each round."""
+
+
+import random
+import time
+
+# List of valid moves for the game. This can be modified to your liking.
+# e.g. You can modify it to the rules of rock-paper-scissors-lizard-spock.
+moves = ['rock','paper','scissors']
