a9c312466838f341e7b396bc4bf190ca4269aa97
Daryl Galvez <daryl.kevin.24@gmail.com>
2020-07-15 20:59:15 +0800

    Feat: Add Player class

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -8,3 +8,13 @@ import time
 # List of valid moves for the game. This can be modified to your liking.
 # e.g. You can modify it to the rules of rock-paper-scissors-lizard-spock.
 moves = ['rock','paper','scissors']
+
+"""The Player class is the parent class for all of the Players in
+this game"""
+
+class Player:
+    def move(self):
+        return moves[0]
+
+    def learn(self, my_move, their_move):
+        pass
